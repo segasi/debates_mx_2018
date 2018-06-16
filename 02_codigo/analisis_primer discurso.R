98969d1847e9b4a567400161dd6bc7f4a97cec91
segasi <segasi@gmail.com>
2018-06-16 09:54:22 -0500

    Realizar diversas transformaciones para generar un dataframe con los diálogos de cada actor: candidatos, moderadores y voz en off

--- 02_codigo/analisis_primer discurso.R
+++ 02_codigo/analisis_primer discurso.R
@@ -30,3 +30,54 @@ primer_debate <- "http://segasi.com.mx/clases/cide/vis_man/datos/primer_debate_c
   download() %>%
   read_docx(skip = 3, remove.empty = TRUE, trim = TRUE)
 
+
+### Transformaciones ----
+
+# Generar un renglón para cada intervención ----
+bd_pd <- primer_debate %>% 
+  str_replace_all("DENISE MAERKER, CONDUCTORA:", "~DENISE MAERKER~") %>%
+  str_replace_all("DENISE MAERKER:", "~DENISE MAERKER~") %>%
+  str_replace_all("AZUCENA URESTI, MODERADORA:", "~AZUCENA URESTI~") %>%
+  str_replace_all("AZUCENA URESTI:", "~AZUCENA URESTI~") %>%
+  str_replace_all("SERGIO SARMIENTO, CONDUCTOR:", "~SERGIO SARMIENTO~") %>%
+  str_replace_all("SERGIO SARMIENTO:", "~SERGIO SARMIENTO~") %>%
+  str_replace_all("VOZ EN OFF:", "~VOZ EN OFF~") %>%
+  str_replace_all("MARGARITA ZAVALA, CANDIDATA INDEPENDIENTE A LA PRESIDENCIA:", "~MARGARITA ZAVALA~") %>%
+  str_replace_all("MARGARITA ZAVALA:", "~MARGARITA ZAVALA~") %>%
+  str_replace_all("JOSÉ ANTONIO MEADE, CANDIDATO PRESIDENCIAL TODOS POR MÉXICO:", "~JOSÉ ANTONIO MEADE~") %>%
+  str_replace_all("JOSÉ ANTONIO MEADE COMO CANDIDATO PRESIDENCIAL DEL PRI:", "~JOSÉ ANTONIO MEADE~") %>%
+  str_replace_all("JOSÉ ANTONIO MEADE:", "~JOSÉ ANTONIO MEADE~") %>%
+  str_replace_all("RICARDO ANAYA, CANDIDATO PRESIDENCIAL POR MÉXICO AL FRENTE:", "~RICARDO ANAYA~") %>%
+  str_replace_all("RICARDO ANAYA:", "~RICARDO ANAYA~") %>% 
+  str_replace_all("ANDRÉS MANUEL LÓPEZ OBRADOR, CANDIDATO PRESIDENCIAL JUNTOS HAREMOS HISTORIA:", "~ANDRÉS MANUEL LÓPEZ OBRADOR~") %>% 
+  str_replace_all("ANDRÉS MANUEL LÓPEZ OBRADOR, CANDIDATO DE LA COALICIÓN `JUNTOS HAREMOS HISTORIA` A LA PRESIDENCIA DE LA REPÚBLICA:", "~ANDRÉS MANUEL LÓPEZ OBRADOR~") %>% 
+  
+  str_replace_all("ANDRÉS MANUEL LÓPEZ OBRADOR:", "~ANDRÉS MANUEL LÓPEZ OBRADOR~") %>% 
+  str_replace_all("ANDRÉS MANUEL PUES OBRADOR:", "~ANDRÉS MANUEL LÓPEZ OBRADOR~") %>%  
+  str_replace_all("JAIME RODRÍGUEZ CALDERÓN, CANDIDATO INDEPENDIENTE A LA PRESIDENCIA:", "~JAIME RODRÍGUEZ CALDERÓN~") %>%
+  str_replace_all("JAIME RODRÍGUEZ CALDERÓN, CANDIDATO INDEPENDIENTE A LA PRESIDENCIA DE LA REPÚBLICA", "~JAIME RODRÍGUEZ CALDERÓN~") %>%
+  str_replace_all("JAIME RODRÍGUEZ CALDERON:", "~JAIME RODRÍGUEZ CALDERÓN~") %>% 
+  str_replace_all("JAIME RODRÍGUEZ CALDERÓN:", "~JAIME RODRÍGUEZ CALDERÓN~") %>% 
+  str_replace_all("JAIME RODRÍGUEZ `EL BRONCO`:", "~JAIME RODRÍGUEZ CALDERÓN~") %>%
+  str_split("~")
+
+# Nombrar la lista como "diálogo" ----
+names(bd_pd) <- "dialogo"
+
+# Guardar la lista como un data frame ----
+bd_pd <- as_data_frame(bd_pd)
+
+# Generar una columna para el nombre de quién habla, reordenar columnas y eliminar espacios en blanco
+bd_pd <- bd_pd %>% 
+  mutate(nombre = ifelse(str_detect(dialogo, "DENISE MAERKER|AZUCENA URESTI|SERGIO SARMIENTO|VOZ EN OFF|MARGARITA ZAVALA|JOSÉ ANTONIO MEADE|RICARDO ANAYA|ANDRÉS MANUEL LÓPEZ OBRADOR|JAIME RODRÍGUEZ CALDERÓN"), dialogo, NA),
+         nombre = lag(nombre)) %>% 
+  filter(!is.na(nombre)) %>% 
+  mutate(rol = ifelse(str_detect(nombre, "DENISE MAERKER|AZUCENA URESTI|SERGIO SARMIENTO"), "Moderador", ifelse(str_detect(nombre, "MARGARITA ZAVALA|JOSÉ ANTONIO MEADE|RICARDO ANAYA|ANDRÉS MANUEL LÓPEZ OBRADOR|JAIME RODRÍGUEZ CALDERÓN"), "Candidato", "Voz en Off"))) %>% 
+  mutate(nombre_corto = case_when(nombre == "ANDRÉS MANUEL LÓPEZ OBRADOR" ~ "López Obrador",
+                                  nombre == "JAIME RODRÍGUEZ CALDERÓN" ~ "El Bronco",
+                                  nombre == "JOSÉ ANTONIO MEADE" ~ "Meade",
+                                  nombre == "MARGARITA ZAVALA" ~ "Zavala",
+                                  nombre == "RICARDO ANAYA" ~ "Anaya")) %>% 
+  mutate(dialogo = str_trim(dialogo, "both")) %>% 
+  select(nombre, nombre_corto, rol, dialogo)  
+
