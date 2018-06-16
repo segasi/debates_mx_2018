ddba390eef328401f1f6c4c02158e979414ec802
segasi <segasi@gmail.com>
2018-06-16 09:51:28 -0500

    Iniciar análisis del primer debate

---
+++ 02_codigo/analisis_primer discurso.R
@@ -0,0 +1,10 @@
+### Paquetes ----
+library(pacman)
+p_load(animation, cowplot, extrafont, forcats, gganimate, 
+       ggforce, ggmap, ggraph, ggrepel, ggridges, hrbrthemes, 
+       igraph, janitor, lubridate, mapdata, maps, maptools, 
+       purrr, readxl, rgdal, rgeos, scales, sp, splitstackshape, 
+       stringi, stringr, stringdist, syuzhet, textreadr, tidyr, 
+       tidygraph, tidytext, tidyverse, tm, treemapify, tweenr, 
+       udpipe, zoo)
+
