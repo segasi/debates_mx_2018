fa604030b6096b29fd015d345ef194bc8e6373e0
segasi <segasi@gmail.com>
2018-06-17 08:10:28 -0500

    Cálculo y gráfica de palabras mencionadas por cada candidato y moderador

--- 02_codigo/analisis_tercer_debate.R
+++ 02_codigo/analisis_tercer_debate.R
@@ -119,3 +119,53 @@ bd_td <- bd_td %>%
   mutate(num_debate = 3)
 
 
+### Análisis usando tidytext ----
+
+# Contar palabras totales por candidato y moderador ----
+bd_td %>% 
+  group_by(nombre) %>% 
+  unnest_tokens(word, dialogo) %>%   
+  count(nombre, word, sort = TRUE) %>%
+  ungroup() %>% 
+  group_by(nombre) %>%
+  summarize(total = sum(n)) %>% 
+  ungroup() %>% 
+  arrange(-total)
+
+# Gráfcia palabras totales de cada actor ----
+bd_td %>% 
+  filter(rol %in% c("Moderador", "Candidato")) %>% 
+  unnest_tokens(word, dialogo) %>%   
+  count(nombre, word, sort = TRUE) %>%
+  ungroup() %>% 
+  group_by(nombre) %>%
+  summarize(total = sum(n)) %>% 
+  ungroup() %>% 
+  mutate(nombre = fct_relevel(nombre, "RICARDO ANAYA", "JOSÉ ANTONIO MEADE", "ANDRÉS MANUEL LÓPEZ OBRADOR", "JAIME RODRÍGUEZ CALDERÓN")) %>% 
+  mutate(pal_por_min = paste(round(total/20.5, 0), "palabras por min. disponible", sep = " "),
+         pal_por_min = ifelse(nombre %in% c("GABRIELA WARKENTIN", "CARLOS PUIG", "LEONARDO CURZIO"), NA, pal_por_min)) %>%  
+  ggplot(aes(reorder(nombre, total), total, fill = nombre)) +
+  geom_col() +
+  geom_text(aes(label = paste("Total: ", comma(total), sep = "")), hjust = 1.15, vjust = -0.6, size = 7, col = "white", fontface = "bold") +
+  geom_text(aes(label = pal_por_min), hjust = 1.12, vjust = 1.35,  size = 6, col = "white") +
+  scale_y_continuous(expand = c(0, 0), limits = c(0, 3800)) +
+  scale_fill_manual(values = c("steelblue", "#f14b4b", "#a62a2a", "#00cdcd", "grey50", "grey50", "grey50")) +
+  labs(title = "NÚMERO DE PALABRAS PRONUNCIADAS POR CADA CANDIDATO\nY MODERADOR EN EL TERCER DEBATE PRESIDENCIAL",
+       subtitle = "El cálculo de palabras por minuto disponible considera que cada candidato tenía hasta 20:30 minutos para hablar.", 
+       x = NULL, 
+       y = NULL, 
+       caption= "\nSebastián Garrido / @segasi / Juan Ricardo Pérez / @juanrpereze / oraculus.mx") +
+  coord_flip() +
+  tema +
+  theme(plot.title = element_text(size = 28),
+        plot.caption = element_text(size = 24), 
+        panel.grid.major = element_blank(),
+        axis.text.x = element_blank(),
+        panel.spacing = unit(2, "lines"), 
+        strip.text = element_text(size = 17, face = "bold", color = "white"),
+        strip.background =element_rect(fill = "#66666680", color = "#66666600"),
+        legend.position = "none")
+
+ggsave(filename = "palabras_por_actor.jpg", path = "03_graficas/palabras/tercero/", width = 15, height = 10, dpi = 100)
+
+
