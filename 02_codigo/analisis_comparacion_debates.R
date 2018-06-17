063277b967ebcd92925271c1bab1f9fa4574ba77
segasi <segasi@gmail.com>
2018-06-17 10:45:46 -0500

    Iniciar análisis comparativo de los tres debates.

---
+++ 02_codigo/analisis_comparacion_debates.R
@@ -0,0 +1,27 @@
+### Paquetes ----
+library(pacman)
+p_load(animation, cowplot, extrafont, forcats, gganimate, 
+       ggforce, ggmap, ggraph, ggrepel, ggridges, hrbrthemes, 
+       igraph, janitor, lubridate, mapdata, maps, maptools, 
+       purrr, readxl, rgdal, rgeos, scales, sp, splitstackshape, 
+       stringi, stringr, stringdist, syuzhet, textreadr, tidyr, 
+       tidygraph, tidytext, tidyverse, tm, treemapify, tweenr, 
+       udpipe, zoo)
+
+### Definir tema de gráficas ----
+tema <-  theme_minimal() +
+  theme(text = element_text(family = "Didact Gothic Regular", color = "grey35"),
+        plot.title = element_text(size = 24, face = "bold", margin = margin(10,0,20,0), family="Trebuchet MS Bold", color = "grey25"),
+        plot.subtitle = element_text(size = 16, face = "bold", colour = "#666666", margin = margin(0, 0, 20, 0), family="Didact Gothic Regular"),
+        plot.caption = element_text(hjust = 0, size = 15),
+        panel.grid = element_line(linetype = 2), 
+        panel.grid.minor = element_blank(),
+        legend.position = "bottom",
+        legend.title = element_text(size = 16, face = "bold", family="Trebuchet MS Bold"),
+        legend.text = element_text(size = 14, family="Didact Gothic Regular"),
+        legend.title.align = 0.5,
+        axis.title = element_text(size = 18, hjust = 1, face = "bold", margin = margin(0,0,0,0), family="Didact Gothic Regular"),
+        axis.text = element_text(size = 16, face = "bold", family="Didact Gothic Regular"))
+
+
+
